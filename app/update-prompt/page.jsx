b72409c770e841948e5bd84ca2cd2443006c77d3
Joseph Akaro <josephmmakaro@gmail.com>
2024-07-26 01:49:28 +0300

    Promptopia going live

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 import Form from '@components/Form';
 
@@ -56,13 +57,15 @@ const EditPrompt = () => {
 	};
 
 	return (
-		<Form
-			type="Edit"
-			post={post}
-			setPost={setPost}
-			submitting={submitting}
-			handleSubmit={updatePrompt}
-		/>
+		<Suspense>
+			<Form
+				type="Edit"
+				post={post}
+				setPost={setPost}
+				submitting={submitting}
+				handleSubmit={updatePrompt}
+			/>
+		</Suspense>
 	);
 };
 
